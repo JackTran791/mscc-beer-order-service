e4fc71f003d0c9b4ad470c2f890c725067a00985
JackTran791 <congsonit@gmail.com>
2020-01-15 21:15:25 -0600

    - Add TastingRoomService and add Spring TaskScheduler.

--- src/main/java/com/luv2code/msccbeerorderservice/repositories/CustomerRepository.java
+++ src/main/java/com/luv2code/msccbeerorderservice/repositories/CustomerRepository.java
@@ -3,10 +3,12 @@ package com.luv2code.msccbeerorderservice.repositories;
 import com.luv2code.msccbeerorderservice.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @author Jack Tran
  */
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    List<Customer> findAllByCustomerNameLike(String tastingRoom);
 }
